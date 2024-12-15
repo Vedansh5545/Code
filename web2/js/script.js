cac26bfbd9692dc5d9c24b9419b7fffed225490c
Vedansh5545 <115136579+Vedansh5545@users.noreply.github.com>
2024-12-14 23:25:08 -0600

    web 2 completed till project section

--- web2/js/script.js
+++ web2/js/script.js
@@ -4,9 +4,9 @@ document.querySelector('.theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 
     if (document.body.classList.contains('dark-theme')) {
-        heroSection.style.backgroundImage = "url('/image/HeroDarkbg.webp')";
+        heroSection.style.backgroundImage = "url('/web2/image/HeroDarkbg.webp')";
     } else {
-        heroSection.style.backgroundImage = "url('/image/herolightbg.webp')";
+        heroSection.style.backgroundImage = "url('/web2/image/herolightbg.webp')";
     }
 });
 
